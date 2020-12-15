2143ad06af30aca7da6aed2cd7a1633c944e4610
LoveDatang <13916991486@163.com>
2020-12-15 14:12:39 +0800

    对mock方法的进一步理解

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # pythonAutoTest
-pythonAutoTest：这个代码仓用来学习python接口自动化测试的内容
-重新打开一个连接会不会需要密
+pythonAutoTest：这个代码仓用来学习python自动化测试的内容
+1. 接口自动化
+2. ui自动化
--- mock/mock_demo.py
+++ mock/mock_demo.py
@@ -1,6 +1,7 @@
 from unittest import mock
 # 这里的mock_method到底有没有用？感觉没有用到，是直接跳过了url，把response_data打印出来了。
 def mock_test(mock_method,request_data,url,method,response_data):
+    # 这个mock_method方法只会返回传入给mock.mock()方法的return_value。可以给这个mock_method传入待测试的函数名，方法名，参数等。
     mock_method = mock.Mock(return_value=response_data)
     response = mock_method(url,method,request_data)
     return response
