ddcc0c51a6b43a519a85850eb98c5c52c90e298e
LoveDatang <13916991486@163.com>
2020-12-15 17:53:25 +0800

    1. 对返回数据的格式进行转化  json,dict,list,str 2. 对返回数据进行校验（包含复杂的返回数据） 3. 找回pycharm中误删的文件

--- base/test_requests.py
+++ base/test_requests.py
@@ -4,11 +4,19 @@ import requests
 
 url1 = "https://api.binstd.com/shouji/query?appkey=7b9b0b62d0f5551e&shouji=13916991486"
 url2 = "https://api.binstd.com/shouji/query"
-data = {
+data1 = {
     "appkey": "7b9b0b62d0f5551e",
     "shouji": "13916991486"
 }
 method = "post"
+
+url3 = "https://api.binstd.com/train/station2s"
+data2 = {
+    "appkey": "7b9b0b62d0f5551e",
+    "start": "杭州",
+    "end": "北京",
+    "ishigh": 0
+}
 
 
 class testRequest():
@@ -34,18 +42,18 @@ class testRequest():
         return response
 
 
-test = testRequest();
+test = testRequest()
 
 test.send_requests(method, url2)
-test.send_post(url2, data)
+test.send_post(url2, data1)
 test.send_get(url1)
-test.choose_method_to_send(url2, data, "post")
+test.choose_method_to_send(url2, data1, "post")
 
 print(test.send_get(url1))
 print((test.send_get(url1).text))
 
 # 知识点：
-# 1. requess.get(),requests.post,requests.request()分别可以使用get，post，或者通过传入调用方法进行相应的接口方式的调用
+# 1. requests.get(),requests.post,requests.request()分别可以使用get，post，或者通过传入调用方法进行相应的接口方式的调用
 # 2. 在函数外定义变量，函数内进行使用
 # 3. 可以使用自定义函数来决定使用哪种调用方法
 # 4. 如果自定义函数有些参数可以没有，那就让在形参列表里给它个默认值None
--- base/test_requests_json.py
+++ base/test_requests_json.py
@@ -6,36 +6,31 @@ url = "https://www.baidu.com/s?"
 data = {
     "wd": "test"
 }
-# 返回格式必须是json格式的才能用
-# 1. response.json()
-# 2. json.loads(response.text)
 response = requests.get(url, data)
 
 url1 = "https://api.binstd.com/train/ticket"
-
 data1 = {
     "appkey": "7b9b0b62d0f5551e",
     "start": "杭州",
     "end": "北京",
     "date": "2020-12-01"
 }
-
 response2 = requests.post(url1, data1)
+
+# 返回str类型的数据
+print(response2.text)
+print(type(response2.text))
+
+# 将字典转成一个json，并且格式化的形式输出
+print(json.dumps(response2.json(), indent=4, ensure_ascii=False))
+
 # 把返回的json类型的值转换成字典类型的值
 print(type(json.loads(response2.text)))
-
-# 返回json格式的数据
-print(response2.text)
-
 # 将json格式的数据转换为字典形式
-print(response2.json())
-
-print("----------------------")
-
+print(type(response2.json()))
 
 # 测试发车的起始点是不是"杭州"和"北京"
 def test_start_end():
-    print("8888888888888")
     print(type(response2.json()))
     print(type(response2.json()["result"]))
     print(type(response2.json()["result"]["list"]))
@@ -44,8 +39,7 @@ def test_start_end():
     # list里面每个值都是字典类型的，所以不存在station值，但是可以通过遍历list中的每个dict型的值得到每个dict的station。
     # * print(type(response2.json()["result"]["list"]["station"]))
 
-    print("8888888888888")
-
+    # 通过xxx[s]取得字典中某个元素's'的值
     for list in response2.json()["result"]["list"]:
         # list中的每个值都是字典类型的。
         print(type(list))
@@ -63,3 +57,12 @@ def test_date():
 
 test_start_end()
 assert 1 == 1
+
+# 将一个json数据转成一个字典
+# 1. response.json()
+# 2. json.loads(response.text)
+
+
+# 将字典转成一个json，并且格式化的形式输出
+# json.dumps(response.json(), indent=4, ensure_ascii=False)
+
